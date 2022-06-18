00141b4bd15bfd9c08441dda16eea4665ebe4087
Joseph Doba <josephdoba@gmail.com>
2022-06-17 18:38:22 -0700

    Add refactoring properties search

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -152,7 +152,63 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 
+//new code:
 const getAllProperties = (options, limit = 10) => {
+  // 1
+  const queryParams = [];
+
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE 1=1
+  `;
+  
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `AND city LIKE $${queryParams.length}`;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night*100, options.maximum_price_per_night*100);
+    queryString += `
+      AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length}
+      `;
+  }
+
+
+  // 4
+  queryString += `
+  GROUP BY properties.id`;
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `
+    HAVING avg(rating) >= $${queryParams.length}
+    `;
+  }
+  queryParams.push(limit);
+  
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  
+  // 5
+  console.log(options);
+  console.log(queryString,queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
+};
+  
+exports.getAllProperties = getAllProperties;
+
+/* //new code:
+const getAllProperties = (options, limit = 10) => {
+  
   return pool.query(`
       SELECT * FROM properties
       LIMIT $1;
@@ -160,17 +216,16 @@ const getAllProperties = (options, limit = 10) => {
   [limit])
     .then((result) => {
       return result.rows;
+
     })
     .catch((err) => {
       console.log(err);
     });
 };
 exports.getAllProperties = getAllProperties;
+*/
 
-
-
-
-/*
+/* old code:
 const getAllProperties = function(options, limit = 10) {
   const limitedProperties = {};
   for (let i = 1; i <= limit; i++) {
