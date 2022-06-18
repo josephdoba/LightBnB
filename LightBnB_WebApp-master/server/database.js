71e84ce17fb7581cabee441b853817bfc2fa1981
Joseph Doba <josephdoba@gmail.com>
2022-06-17 19:26:22 -0700

    Created Add Property SQL query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -154,18 +154,15 @@ exports.getAllReservations = getAllReservations;
 
 //new code:
 const getAllProperties = (options, limit = 10) => {
-  // 1
   const queryParams = [];
 
-  // 2
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
   WHERE 1=1
   `;
-  
-  // 3
+
   if (options.city) {
     queryParams.push(`%${options.city}%`);
     queryString += `AND city LIKE $${queryParams.length}`;
@@ -178,8 +175,6 @@ const getAllProperties = (options, limit = 10) => {
       `;
   }
 
-
-  // 4
   queryString += `
   GROUP BY properties.id`;
 
@@ -196,11 +191,9 @@ const getAllProperties = (options, limit = 10) => {
   LIMIT $${queryParams.length};
   `;
   
-  // 5
   console.log(options);
   console.log(queryString,queryParams);
 
-  // 6
   return pool.query(queryString, queryParams).then((res) => res.rows);
 };
   
@@ -241,6 +234,59 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
+
+// new code:
+
+const addProperty = function(property) {
+  const propertyParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+
+  let propertyString = `
+    INSERT INTO properties (
+      owner_id, 
+      title, 
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms)
+    VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13, $14)
+    RETURNING *;
+    `;
+  
+  return pool.query(propertyString, propertyParams)
+    .then(res => {
+      console.log(res.rows);
+      return res.rows;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+exports.addProperty = addProperty;
+
+/* old code:
 const addProperty = function(property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
@@ -248,3 +294,4 @@ const addProperty = function(property) {
   return Promise.resolve(property);
 };
 exports.addProperty = addProperty;
+*/
